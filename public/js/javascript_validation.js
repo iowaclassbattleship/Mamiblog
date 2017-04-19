eec4f2c9c4336b19e0da9ecf084600b68d86dcdb
Iowa Class Battleship <marc@schlaepfer.org>
2017-04-19 14:55:35 +0200

    JQuery Registration Validation

--- public/js/javascript_validation.js
+++ public/js/javascript_validation.js
@@ -1,10 +1,45 @@
 $(document).ready(function() {
     var mailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var nameRegex = /^[a-zA-Z]+$/;
+    var pwdRegex = /^([A-Za-z]{8,})\w+$/;
 
-    $("#email").change(function () {
-        var emailValue = $("#email").val();
+    $('#firstName').keyup(function () {
+       var firstNameValue = $('#firstName').val();
+        if(!firstNameValue.match(nameRegex)){
+            $('#firstName').css('background-color', '#ffcccc');
+        }
+        else {
+            $('#firstName').css('background-color', '#b3ffb3');
+        }
+    });
+
+    $('#lastName').keyup(function () {
+        var firstNameValue = $('#lastName').val();
+        if(!firstNameValue.match(nameRegex)){
+            $('#lastName').css('background-color', '#ffcccc');
+        }
+        else {
+            $('#lastName').css('background-color', '#b3ffb3');
+        }
+    });
+
+    $('#email').keyup(function () {
+        var emailValue = $('#email').val();
         if(!emailValue.match(mailRegex)){
-            $('#email').parent().css('border-color','red');
+            $('#email').css('background-color', '#ffcccc');
+        }
+        else {
+            $('#email').css('background-color', '#b3ffb3');
+        }
+    });
+
+    $('#password').keyup(function () {
+        var passwordValue = $('#password').val();
+        if(!passwordValue.match(pwdRegex)){
+            $('#password').css('background-color', '#ffcccc');
+        }
+        else {
+            $('#password').css('background-color', '#b3ffb3');
         }
     });
 });

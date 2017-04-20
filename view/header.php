c97cf95b8932cdc19d007034aab58d3910c1fc07
bdrozd <dominicdroz1999@gmail.com>
2017-04-20 09:30:49 +0200

    header update

--- view/header.php
+++ view/header.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
     <!-- Custom styles for this template -->
       <link href="/js/javascript.js" rel="script">
 
@@ -16,8 +17,6 @@
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-      <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"></script>
     <![endif]-->
   </head>
   <?php if(isset($_SESSION['user'])) : ?>

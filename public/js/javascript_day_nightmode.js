37f17bd779e33f091407df82dff9d0e968860552
bdrozd <dominicdroz1999@gmail.com>
2017-04-20 15:19:08 +0200

    day night mode perfek

--- public/js/javascript_day_nightmode.js
+++ public/js/javascript_day_nightmode.js
@@ -1,9 +1,19 @@
 
 $(document).ready(function() {
 
+    $('body').addClass('notransition');
+
+    if($.cookie("isNight") == "true")
+        $('body').addClass('night');
+
+
+
+
     $('#day_nightmode').click(changeMode);
 
     function changeMode() {
+        $('body').removeClass('notransition');
         $('body').toggleClass('night');
+        $.cookie("isNight", $('body').hasClass('night'));
     }
 });

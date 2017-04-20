73bbc8361f88a466789048b0f204270508a4127b
Iowa Class Battleship <marc@schlaepfer.org>
2017-04-20 11:53:46 +0200

    useless trash

--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+      <link rel="icon" href="http://cdn.mysitemyway.com/icons-watermarks/flat-circle-white-on-black/alphanum/alphanum_lowercase-letter-m/alphanum_lowercase-letter-m_flat-circle-white-on-black_512x512.png">
     <title><?= $title ?> | Mamiblog</title>
 
     <!-- Bootstrap core CSS -->

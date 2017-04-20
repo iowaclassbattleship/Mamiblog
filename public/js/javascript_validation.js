6a0301b2bd16513c69d3c8093170b3bc5ea56a69
Iowa Class Battleship <marc@schlaepfer.org>
2017-04-20 14:54:59 +0200

    Regex imprevement

--- public/js/javascript_validation.js
+++ public/js/javascript_validation.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var mailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{1,}))$/;
+    var mailRegex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
     var pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     var submitButton = false;
     var submitButton2 = false;
@@ -22,7 +22,7 @@ $(document).ready(function() {
         }
     }
 
-    $('#email').keypress(validateEmail);
+    $('#email').keyup(validateEmail);
     $('#email').change(validateEmail);
 
     function validatePassword () {
@@ -37,7 +37,7 @@ $(document).ready(function() {
         }
     }
 
-    $('#password').keypress(validatePassword);
+    $('#password').keyup(validatePassword);
     $('#password').change(validatePassword);
 
 

b13053007da2af12534957efdead7576ab2d5861
Iowa Class Battleship <marc@schlaepfer.org>
2017-04-20 15:23:06 +0200

    Bschlm

--- public/js/javascript_validation.js
+++ public/js/javascript_validation.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     var mailRegex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
     var pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    var submitButton = false;
-    var submitButton2 = false;
 
     /*
         Validierung von E-Mail und Passworteingabe.
@@ -14,14 +12,13 @@ $(document).ready(function() {
         var emailValue = $('#email').val();
         if(!emailValue.match(mailRegex)){
             $('#email').css('background-color', '#ffcccc');
-            submitButton = true;
         }
         else {
             $('#email').css('background-color', '#b3ffb3');
-            submitButton = false;
         }
     }
 
+    $('#email').keydown(validateEmail);
     $('#email').keyup(validateEmail);
     $('#email').change(validateEmail);
 
@@ -29,32 +26,19 @@ $(document).ready(function() {
         var passwordValue = $('#password').val();
         if(!passwordValue.match(pwdRegex)){
             $('#password').css('background-color', '#ffcccc');
-            submitButton2 = true;
         }
         else {
             $('#password').css('background-color', '#b3ffb3');
-            submitButton2 = false;
         }
     }
 
+    $('#password').keydown(validatePassword);
     $('#password').keyup(validatePassword);
     $('#password').change(validatePassword);
 
 
 
     $(':input[type="submit"]').prop('disabled', true);
-    $('#password').change(validateSubmitButton);
-    $('#password').change(validateSubmitButton);
-
-    function validateSubmitButton() {
-
-        if ($(submitButton = true) && $(submitButton2 = true)) {
-            $(':input[type="submit"]').prop('disabled', false);
-        }
-        else {
-            $(':input[type="submit"]').prop('disabled', true);
-        }
-    }
 });
 
 

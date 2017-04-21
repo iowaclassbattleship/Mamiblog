28f07619fb204726e845491520ca2af7626386b9
bdrozd <dominicdroz1999@gmail.com>
2017-04-21 08:22:44 +0200

    upload title fixed

--- view/blog_create.php
+++ view/blog_create.php
@@ -1,5 +1,5 @@
-
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/style.css">
 <form class="form-horizontal" action="/blog/doCreate" method="post" enctype="multipart/form-data">
     <div class="component" data-html="true">
         <div class="form-group">
@@ -11,7 +11,8 @@
         <div class="form-group">
             <label class="col-md-2 control-label" for="title">Title</label>
             <div class="col-md-4">
-                <input id="title" name="title" type="textarea" placeholder="Title" rows="4" cols="50" required>
+                <input id="title" name="title" type="text" placeholder="Title" class="form-control input-md" required>
+
             </div>
         </div>
         <div class="form-group">

f058dda20cdae6a8a8bfc30c068259dae273df53
bdrozd <dominicdroz1999@gmail.com>
2017-04-21 08:42:07 +0200

    database update

--- data/mvc.sql
+++ data/mvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 20. Apr 2017 um 13:42
+-- Erstellungszeit: 21. Apr 2017 um 08:40
 -- Server-Version: 10.1.21-MariaDB
 -- PHP-Version: 5.6.30
 
@@ -27,27 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `blog` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(10) UNSIGNED NOT NULL,
   `picture` varchar(100) NOT NULL,
   `title` varchar(100) NOT NULL,
   `date` varchar(100) NOT NULL,
   `creator` varchar(100) NOT NULL,
   `private` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user`
---
-
-CREATE TABLE `user` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `firstName` varchar(64) NOT NULL,
-  `lastName` varchar(64) NOT NULL,
-  `email` varchar(128) NOT NULL UNIQUE,
-  `password` varchar(512) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,19 +42,26 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `comments` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `blogid` int UNSIGNED NOT NULL,
-  `userid` int UNSIGNED NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
+  `blogid` int(10) UNSIGNED NOT NULL,
+  `userid` int(10) UNSIGNED NOT NULL,
   `comment` varchar(400) NOT NULL,
-  `time` date NOT NULL,
-  FOREIGN KEY (blogid) REFERENCES blog(id),
-  FOREIGN KEY (userid) REFERENCES user(id)
+  `time` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
+--
+-- Tabellenstruktur für Tabelle `user`
+--
 
-
-
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `firstName` varchar(64) NOT NULL,
+  `lastName` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `user`
@@ -77,3 +69,62 @@ CREATE TABLE `comments` (
 
 INSERT INTO `user` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
 (1, 'Admin', 'Mamiblog', 'admin.mamiblog@gmail.com', '$2y$10$lCccwBR4EvmN8N80wFOquusAEMGRfNx4cSPHBNYAv1bsNJlXsVAFy');
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `comments_ibfk_1` (`blogid`),
+  ADD KEY `comments_ibfk_2` (`userid`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT für Tabelle `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT für Tabelle `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`blogid`) REFERENCES `blog` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
